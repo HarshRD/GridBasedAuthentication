c3c87e94727f92da974ba047c41909422d857ad3
Harsh Desai <harsh4787@gmail.com>
2017-03-08 15:05:44 -0600

    Add files via upload

---
+++ Finance Appln/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Finance Appln")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("PROIT")> 
+<Assembly: AssemblyProduct("Finance Appln")> 
+<Assembly: AssemblyCopyright("Copyright © PROIT 2007")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("48a71cd0-c1bb-4e4b-b54e-27afa74a025b")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
